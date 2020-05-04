be82ef8ced5f896bb303b19fbd908739ac8f52f4
Ruan Gabriel <ruangabrielsac12@gmail.com>
2020-05-04 02:33:50 -0300

    Criando a operação de instanciação do banco H2

--- src/main/java/br/com/ruan/cursomc/CursomcApplication.java
+++ src/main/java/br/com/ruan/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package br.com.ruan.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.ruan.cursomc.model.CategoriaModel;
+import br.com.ruan.cursomc.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		CategoriaModel cat1 = new CategoriaModel(null, "Informática");
+		CategoriaModel cat2 = new CategoriaModel(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
